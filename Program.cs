a3ef31bca60316ce2f0ef29a3dfedaf32111e8bc
Rob Galvan <robgalvan@gmail.com>
2017-06-08 20:23:41 -0500

    updated tuple

--- Program.cs
+++ Program.cs
@@ -31,8 +31,8 @@ namespace Game
             Console.WriteLine("you look around and see 3 things you can use as a weapon");
             Console.WriteLine("which weapon do you want to pick?");
 
-
-            Weapons weapon = new Weapons();
+            
+            //Weapons weapon = new Weapons();
             string[,] weapons1 = new string[3, 2] { { "Torch (it)", "1000" }, { "Freeze (make harder)", "100" }, { "Candle (burn with prayers)", "500" } };
             string[,] weapons2 = new string[3, 2] { { "Spoon (her)", "1000" }, { "Whip Cream (on it)", "100" }, { "Fork (it)", "500" } };
             string[,] weapons3 = new string[3, 2] { { "Toothpick (poker face)", "1000" }, { "Vodka (martini)", "100" }, { "Mouth (smash)", "500" } };
@@ -59,10 +59,10 @@ namespace Game
 
 
 
-            var stages = new List<Tuple<Alien, Hero, string[,]>>();
-            stages.Add(Tuple.Create(alien1, hero, weapons1));
-            stages.Add(Tuple.Create(alien2, hero, weapons2));
-            stages.Add(Tuple.Create(alien3, hero, weapons3));
+            var stages = new List<Tuple<Alien, Hero, string[,],string>>();
+            stages.Add(Tuple.Create(alien1, hero, weapons1,alienDraw1()));
+            stages.Add(Tuple.Create(alien2, hero, weapons2,alienDraw2()));
+            stages.Add(Tuple.Create(alien3, hero, weapons3,alienDraw3()));
 
             foreach (var stage in stages)
             {
@@ -238,16 +238,18 @@ namespace Game
 
         }
 
-        public static bool stageCompleted(Tuple<Alien, Hero, string[,]> stage)
+        public static bool stageCompleted(Tuple<Alien, Hero, string[,],string> stage)
         {
 
             //Define your variables
             var alien1 = stage.Item1;
             var hero = stage.Item2;
             var weapons1 = stage.Item3;
+            var enemyDraw = stage.Item4;
 
             while (alien1.healthPoints > 0 && hero.lives > 0)
             {
+                Console.WriteLine(enemyDraw);
                 Console.WriteLine("Choice A = {0} , or , Choice B {1} , or , Choice C {2}", weapons1[0, 0], weapons1[1, 0], weapons1[2, 0]);
                 string selection = Console.ReadLine();
                 int points = 0;
@@ -303,9 +305,9 @@ namespace Game
 
 
 
-        public static void alienDraw1()
+        public static string alienDraw1()
         {
-            string alien1 = @"
+            return @"
    _...___
  .'_ (.) _'.
 /  (.) (.)  \
@@ -314,12 +316,12 @@ namespace Game
  '_+PATRIK+.'-_-_-\_\
  __//   \\__
 (___)   (___)";
-            Console.WriteLine(alien1);
+            
         }
 
-        public static void alienDraw2()
+        public static string alienDraw2()
         {
-            string alien2 = @"
+            return @"
     /\.-.../`\     /#\
    /_\ /_   \ \--.  \ \
  / /.\ /.\   \ \--. / /
@@ -328,12 +330,12 @@ namespace Game
  \  \/ \/  ; ./ |  \--\
   ##__GAGA____##|...\--\     
   (,,,)(,,,)   (,,,)(,,,)";
-            Console.WriteLine(alien2);
+            
         }
 
-        public static void alienDraw3()
+        public static string alienDraw3()
         {
-            string alien3 = @"	
+            return @"	
 		
       __.           __.
   __ {_/        __ {_/ 
@@ -346,7 +348,7 @@ namespace Game
  '.         .''.           .'
    '-.....-'   '-........-'
 ";
-            Console.WriteLine(alien3);
+            
         }
     }
 
@@ -398,11 +400,11 @@ namespace Game
         }
     }
 
-    public class Weapons
-    {
-        string[,] weapons1 = new string[3, 2] { { "Spoon (her)", "9000" }, { "Whip Cream (on it)", "1" }, { "Fork (it)", "25" } };
-        string[,] weapons2 = new string[3, 2] { { "Torch (it)", "9000" }, { "Freeze (make harder)", "1" }, { "Candle (burn with prayers)", "5" } };
-        string[,] weapons3 = new string[3, 2] { { "Toothpick (poker face)", "9000" }, { "Vodka (martini)", "1" }, { "Mouth (smash)", "15" } };
-    }
+    // public class Weapons
+    // {
+    //     string[,] weapons1 = new string[3, 2] { { "Spoon (her)", "9000" }, { "Whip Cream (on it)", "1" }, { "Fork (it)", "25" } };
+    //     string[,] weapons2 = new string[3, 2] { { "Torch (it)", "9000" }, { "Freeze (make harder)", "1" }, { "Candle (burn with prayers)", "5" } };
+    //     string[,] weapons3 = new string[3, 2] { { "Toothpick (poker face)", "9000" }, { "Vodka (martini)", "1" }, { "Mouth (smash)", "15" } };
+    // }
 
 }
